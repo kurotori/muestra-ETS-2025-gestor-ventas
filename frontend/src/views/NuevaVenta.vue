a2f5065ee9928dd3cfc85bb27c7acac40819b585
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2025-10-29 22:49:15 -0300

    feat: Añadir botón para agregar productos en la vista Nueva Venta y mejorar la estructura de la sección de detalles

--- frontend/src/views/NuevaVenta.vue
+++ frontend/src/views/NuevaVenta.vue
@@ -20,27 +20,45 @@ import Encabezado from '../components/base/Encabezado.vue';
                 Nueva Venta
             </h2>
 
-            <div class="
+            <div class="detalle
                     w-full h-9/10
                     mt-2.5
                     border border-fondo-100
                     bg-white
                     rounded-2xl
-                    detalle">
-                <div class="btnAgregarProd
-                        w-[20vw] h-[5vh]
-                        rounded-2xl
-                        border-black border-2
-                        text-center
-                        m-1.5
-                        hover:bg-black hover:text-white
-                        
-                        ">
-                    <span class="material-symbols-outlined h-2.5 ">
-                        add
-                    </span>
-                    <span class="h-full inline-block align-middle">Agregar Producto</span> 
+                    ">
+                <div class="detalleProductos">
+                    <div class="botoneraDetalleProductos
+                            border-black border-b-1
+                            ">
+                        <div class="btnAgregarProd
+                                relative
+                                flex justify-between items-center
+                                w-[20vw] h-[5vh]
+                                rounded-2xl
+                                border-black border-2
+                                text-center
+                                m-1.5 p-2.5
+                                hover:bg-black hover:text-white
+                                cursor-pointer
+                            ">
+                            <span class="material-symbols-outlined 
+                                    h-[25px] aspect-square
+                                    left-0 right-auto
+                                ">
+                                add
+                            </span>
+                            <span>
+                                Agregar Producto
+                            </span>
+                        </div>
+                    </div>
+                    <div class="listaDetalleProductos">
+
+                    </div>
                 </div>
+
+
             </div>
 
         </div>
