82e3f7bc5a50f02675aedc84d54191d6c36f9f8a
Luis Sebastián de los Ángeles <luissebastianangeles@gmail.com>
2025-10-30 08:27:06 -0300

    Se agrega un pquete extra al sistema

--- frontend/src/components/venta/ProductoVenta.vue
+++ frontend/src/components/venta/ProductoVenta.vue
@@ -1,9 +1,17 @@
 <script setup>
+    import { ref } from 'vue';
+
+    const productos = ref([])
 </script>
 
 <template>
-    <div>
-
+    <div class="datosProducto
+            w-full h-fit
+            border-black border-b
+            ">
+        <select name="producto" id="producto">
+            <option value="cosa1">Cosa 1</option>
+        </select>
     </div>
 </template>
 
