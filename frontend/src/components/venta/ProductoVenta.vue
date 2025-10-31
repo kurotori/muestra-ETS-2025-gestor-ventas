e6ad756e890df8b67332c852838c851e07f2f162
Luis Sebastián de los Angeles <luissebastianangeles@gmail.com>
2025-10-30 21:18:22 -0300

    feat: Añadir productos predeterminados y mejorar la estructura de selección en ProductoVenta

--- frontend/src/components/venta/ProductoVenta.vue
+++ frontend/src/components/venta/ProductoVenta.vue
@@ -1,27 +1,44 @@
 <script setup>
 import { ref } from 'vue';
 
-const productos = ref([])
+const productos = ref([
+    {
+        id: 0,
+        nombre: 'Cubre webcam',
+        precio: 50
+    },
+    {
+        id: 1,
+        nombre: 'Mini catapulta',
+        precio: 150
+    }
+])
+
+const productoSel = ref('')
 </script>
 
 <template>
     <div class="datosProducto
-            w-95/100 h-fit
-            flex flex-row 
+            w-full h-fit
+            grid grid-cols-[minmax(150px,2fr)_minmax(50px,1fr)_minmax(50px,1fr)_minmax(50px,1fr)] gap-1.5 
+
             ">
-        <select name="producto" id="producto" class=" flex-2">
-            <option value="cosa1">Cosa 1</option>
-            <option value="cosa2">Cosa 2</option>
+        <select v-model="productoSel" name="producto" id="producto" class="">
+            <option v-for="producto in productos" :value="producto.id">{{ producto.nombre }}</option>
         </select>
-        <input type="number" id="cantidad" class="flex-1">
-        <input type="number" id="precio">
-        <input type="number" id="subtotal">
+        <input type="number" id="cantidad" class="">
+        <span>{{ productos[productoSel].nombre }}</span>
+        <span>0</span>
     </div>
 </template>
 
 <style scoped>
 input {
-    margin-left: 5px;
+    /*margin-left: 5px;*/
     border-bottom: 1px solid black;
+}
+
+.datosProducto>* {
+    text-align: center;
 }
 </style>
